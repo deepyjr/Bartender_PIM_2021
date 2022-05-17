cfe322850f8b4d77d59b3496b1ea84ef64cc8322
Raphael <deepyjr@gmail.com>
2022-05-17 14:48:20 +0200

    update fornt

--- src/Views/CartContent/CartContent.jsx
+++ src/Views/CartContent/CartContent.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React from 'react';
+import './CartContent.css';
+import {CartContext} from '../../Store/CartContext'
+import axios from "axios";
 
 function CartContent() {
+  const [lineItems, setLineItems] = React.useState();
+  const [refreshComponent, setRefreshComponent] = React.useState(true);
+  const { cartState, cartDispatch } = React.useContext(CartContext);
+
+
+  React.useEffect(() => {
+    const callApi = () => {
+      setRefreshComponent(true);
+         console.log(cartState.userCart)
+      // axios
+      //   .post(baseUrl + "/folderDetail", { path: pathState.path })
+      //   .then(function (response) {
+      //     // handle success
+      //     setRefreshComponent(true);
+      //     setLineItems(
+      //       response.data.map((file, index) => {
+      //         return (
+      //           <div>test</div>
+      //         );
+      //       })
+      //     );
+      //   })
+      //   .catch(function (error) {
+      //     // handle error
+      //     console.log(error);
+      //   });
+    };
+
+    if (refreshComponent) {
+      callApi();
+      setRefreshComponent(false);
+    }
+  }, refreshComponent);
+
   return (
-    <div className="cart-content">CartContent</div>
+    <div className="cart-content">
+
+    <h2>Mon panier</h2>
+      <div className="container-cart">
+        {lineItems}
+      </div>
+    </div>
   )
 }
 
