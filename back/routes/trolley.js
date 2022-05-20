e9d0ac8a1994727f8e51256ceb075f95f9fecab1
Florian GILLET <florian.gillet@estiam.com>
2022-05-20 14:30:55 +0200

    trolley

--- back/routes/trolley.js
+++
@@ -1,6 +0,0 @@
-TrolleyController = require('../controllers/trolley-controller');
-module.exports = (server) => {
-    server.get('/trolleys', TrolleyController.readAll);
-    server.post('/trolley', TrolleyController.create);
-    server.delete('/trolley', TrolleyController.deleteAll);
-}
