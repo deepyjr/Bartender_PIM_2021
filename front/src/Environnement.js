c57838ee546cb539e5ce74c7a10a202c9a29871e
Raphael <deepyjr@gmail.com>
2022-08-24 23:14:09 +0200

    add front update for the cart

--- front/src/Environnement.js
+++ front/src/Environnement.js
@@ -1,10 +1,10 @@
 
 const Name = "Bartender"
-
+const BackAddress = "http://localhost:8080"
 const Color1 = "#0B162C"
 const Color2 = "#1C2942"
 const Color3 = "#3B556D"
 const Color4 = "#5FC2BA"
 const Color5 = "#FFFFFF"
 
-export {Name, Color1, Color2, Color3, Color4, Color5 }
+export {Name, Color1, Color2, Color3, Color4, Color5, BackAddress }
--- front/src/Views/CartContent/CartContent.jsx
+++ front/src/Views/CartContent/CartContent.jsx
@@ -3,52 +3,37 @@ import './CartContent.css';
 import {CartContext} from '../../Store/CartContext'
 import axios from "axios";
 import CartItem from "../../Components/CartItem/cartItem";
+import Button from "../../Components/Button/Button";
+import {BackAddress} from "../../Environnement";
 
 function CartContent() {
   const [lineItems, setLineItems] = React.useState();
   const [refreshComponent, setRefreshComponent] = React.useState(true);
   const { cartState, cartDispatch } = React.useContext(CartContext);
 
-
-  React.useEffect(() => {
-    const callApi = () => {
-      setRefreshComponent(true);
-         console.log(cartState.userCart)
-        
-      // axios
-      //   .post(baseUrl + "/folderDetail", { path: pathState.path })
-      //   .then(function (response) {
-      //     // handle success
-      //     setRefreshComponent(true);
-      //     setLineItems(
-      //       response.data.map((file, index) => {
-      //         return (
-      //           <div>test</div>
-      //         );
-      //       })
-      //     );
-      //   })
-      //   .catch(function (error) {
-      //     // handle error
-      //     console.log(error);
-      //   });
+    const sendDataToTheBack = () => {
+      axios
+        .post(BackAddress + "/cart", cartState.userCart)
+        .then(function (response) {
+          // handle success
+          console.log(response.data)
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        });
     };
-
-    if (refreshComponent) {
-      callApi();
-      setRefreshComponent(false);
-    }
-  }, refreshComponent);
 
   return (
     <div className="cart-content">
 
     <h2>Mon panier</h2>
       <div className="container-cart">
-        {/* { cartState.userCart.map((cartItem,index) =>{
+        { cartState.userCart.map((cartItem,index) =>{
             return(<CartItem name={cartItem.name} price={cartItem.price} quantity={cartItem.quantity}></CartItem>)
-         })} */}
+         })}
       </div>
+      <button className="custom-button" onClick={()=>{sendDataToTheBack()}}>Finaliser ma commande</button>
     </div>
   )
 }
