37e71cc09a6621c31f97593577f899c5ecc26835
WilliamH <58976208+Willix-IT@users.noreply.github.com>
2022-09-08 15:37:07 +0200

    add notif when order

--- front/src/Components/Wrapper/Wrapper.jsx
+++ front/src/Components/Wrapper/Wrapper.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import "./Wrapper.css";
 import { CartContext } from "../../Store/CartContext";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 function Wrapper(props) {
-
   const { cartState, cartDispatch } = React.useContext(CartContext);
+  const [open, setOpen] = React.useState(false);
+  const [itemAdded, setItemAdded] = React.useState("");
+
+  const handleClick = (item) => {
+    setItemAdded(item);
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const action = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
 
   return (
     <div className="container-wrapper">
@@ -29,17 +59,22 @@ function Wrapper(props) {
       <div className="contanier-items-suggestion">
         {props.items.map((item, index) => {
           return (
-            <div className="items-suggestion" key={index} onClick={()=>{
-              cartDispatch({
-                type:"addToCart",
-                payload:{
-                  name: item.name,
-                  description: item.description,
-                  price: item.prix,
-                  quantity: 1
-                }
-              })
-            }}>
+            <div
+              className="items-suggestion"
+              key={index}
+              onClick={() => {
+                cartDispatch({
+                  type: "addToCart",
+                  payload: {
+                    name: item.name,
+                    description: item.description,
+                    price: item.prix,
+                    quantity: 1,
+                  },
+                });
+                handleClick(item.name);
+              }}
+            >
               <div className="left-part-suggestion">
                 <h3>{item.name}</h3>
                 <p>{item.description}</p>
@@ -51,6 +86,13 @@ function Wrapper(props) {
           );
         })}
       </div>
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message={itemAdded + " ajouté à la commande"}
+        action={action}
+      />
     </div>
   );
 }
